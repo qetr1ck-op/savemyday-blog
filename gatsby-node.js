611264e885f80bf68804af62727e263678225f63
qetr1ck-op <orestprustayko@gmail.com>
2019-08-23 17:57:31 +0300

    async and static rendering

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,63 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+const createPokemonPages = async ({ actions }) => {
+  console.log(actions)
+  const allPokemon = await Promise.resolve([
+    { name: "pikachu" },
+    { name: "charizard" },
+    { name: "squirtle" },
+  ])
+  // Create a page that lists all Pokémon.
+  actions.createPage({
+    path: `/all-pokemon`,
+    component: require.resolve("./src/templates/all-pokemon.js"),
+    context: { allPokemon },
+  })
+  // Create a page for each Pokémon.
+  allPokemon.forEach(pokemon => {
+    actions.createPage({
+      path: `/pokemon/${pokemon.name}/`,
+      component: require.resolve("./src/templates/pokemon.js"),
+      context: { pokemon },
+    })
+  })
+}
+
+const createBlogPostPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/pages/blog-template.js`)
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: blogPostTemplate,
+      context: {}, // additional data can be passed via context
+    })
+  })
+}
+
+exports.createPages = async options => {
+  await Promise.all([createPokemonPages(options), createBlogPostPages(options)])
+}
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+export const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
@@ -12,5 +12,3 @@ const SecondPage = () => (
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
-
-export default SecondPage
