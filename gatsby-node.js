098e311b25a8b267672dc59f9a0b0aceebbe3f47
qetr1ck-op <orestprustayko@gmail.com>
2019-12-15 09:59:39 +0200

    upd config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,11 @@
+// write the gatsby config in TypeScript
+// https://github.com/gatsbyjs/gatsby/issues/1457
 require("source-map-support").install()
 require("ts-node").register()
 
 const { createPages, onCreateNode } = require("./gatsby-node-config")
 
-exports.createPages = createPages
-
-exports.onCreateNode = onCreateNode
+module.exports = {
+  createPages,
+  onCreateNode,
+}
